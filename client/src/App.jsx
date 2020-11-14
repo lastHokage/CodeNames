7b3895a52553446a132453a911764a45b176ab92
lastHokage <amir.gevari@gmail.com>
2020-11-14 20:22:41 +0000

    feat: add the generator to the app and pass them to the wordCard component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./App.css";
 import WordCard from "./components/WordCard";
+import { generator } from "./helpers/generator";
 
 function App() {
+  const {
+    redCardsPosition,
+    blueCardsPosition,
+    civiliansCardsPosition,
+    assassinCardPosition,
+  } = generator();
   return (
     <div className="App">
       This is codenames game
       <div className="word-container">
-        <WordCard />
+        <WordCard
+          red={redCardsPosition}
+          blue={blueCardsPosition}
+          yellow={civiliansCardsPosition}
+          black={assassinCardPosition}
+        />
       </div>
     </div>
   );
