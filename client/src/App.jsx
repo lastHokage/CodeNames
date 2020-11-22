392e1ba026e6d4f58347eb6dac5f686e0a316523
lastHokage <amir.gevari@gmail.com>
2020-11-22 18:53:56 +0000

    feat: create a game component as home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import "./App.css";
-import WordCard from "./components/WordCard";
-import { generator } from "./helpers/generator";
+import "./assets/main.css";
+import Game from "./components/Game";
 
 function App() {
-  const {
-    redCardsPosition,
-    blueCardsPosition,
-    civiliansCardsPosition,
-    assassinCardPosition,
-  } = generator();
   return (
     <div className="App">
-      This is codenames game
-      <div className="word-container">
-        <WordCard
-          red={redCardsPosition}
-          blue={blueCardsPosition}
-          yellow={civiliansCardsPosition}
-          black={assassinCardPosition}
-        />
-      </div>
+      <Game />
     </div>
   );
 }
