32489ee9bc1f8b28ee224b65cbea17fe0ec9499d
lastHokage <amir.gevari@gmail.com>
2021-04-28 16:01:27 +0100

    modify the npm start script

--- server.js
+++ server.js
@@ -144,6 +144,14 @@ io.on("connection", (socket) => {
   });
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 http.listen(port, () => {
   console.log(`app is runnig on http://localhost:${port}`);
 });
